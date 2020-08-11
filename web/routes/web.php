267586a11255f6235e2da1f7ae7db33a489bcf3a
Fabian Rojas <fnra027@gmail.com>
2020-08-11 00:14:56 -0400

    feat(Auth): add front end to user auth

--- web/routes/web.php
+++ web/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::redirect('/', 'login');
+Route::get('login', 'Auth\LoginController@login')->name('login');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
